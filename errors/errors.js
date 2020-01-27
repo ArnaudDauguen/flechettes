0b8f8822b4cff687251eb192b565cffe0e553aee
arnaud d <arnaud.dauguen@ynov.com>
2020-01-27 21:09:37 +0100

    errors

---
+++ errors/errors.js
@@ -0,0 +1,44 @@
+class HttpError extends Error {
+    toJSON() {
+        const stack = process.env.NODE_ENV === 'development' ? this.stack : undefined
+        return {
+            type: this.type || 'SERVER_ERROR',
+            message: this.message || 'Server Error',
+            stack
+        }
+    }
+}
+
+class NotFoundError extends HttpError {
+    constructor(message = 'Not found', type = 'NOT_FOUND') {
+        super(message)
+        this.status = 404
+        this.type = type
+    }
+}
+  
+class BadRequestError extends HttpError {
+    constructor(message = 'Bad request', type = 'BAD_REQUEST') {
+        super(message)
+        this.status = 400
+        this.type = type
+    }
+}
+  
+class NotAcceptableError extends HttpError {
+    constructor(message = 'Not Acceptable', type = 'NOT_ACCEPTABLE') {
+        super(message)
+        this.status = 406
+        this.type = type
+    }
+}
+  
+class ServerError extends HttpError {
+    constructor(message = 'Server Error', type = 'SERVER_ERROR') {
+        super(message)
+        this.status = 500
+        this.type = type
+    }
+}
+
+module.exports = { NotFoundError, BadRequestError, NotAcceptableError, ServerError }
--- router.js
+++ router.js
@@ -1,62 +1,66 @@
 const env = process.env
 const PORT = env.PORT || 3000
+const { ServerError, NotAcceptableError } = require('./errors/errors.js')
 
 const express = require('express');
 const router = express();
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
-const api = express()
-api.set('view engine', 'hbs')
-api.set('views', __dirname + '/views')
+const app = express()
+app.set('view engine', 'hbs')
+app.set('views', __dirname + '/views')
 
 
 //body parsing
-api.use(bodyParser.json())
-api.use(bodyParser.urlencoded({ extended: false }))
-api.use(methodOverride('_method'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
 
-api.use('/players', require('./routers/players.js'))
-api.use('/games', require('./routers/games.js'))
+app.use('/players', require('./routers/players.js'))
+app.use('/games', require('./routers/games.js'))
 
 //TODO return static files
-// api.get('*', (req, res, next) => {
+// app.get('*', (req, res, next) => {
 //     res.redirect(301, '/games')
 // })
 
-api.all('/', (req, res, next) => {
-    res.format({
-        html: function(){
-            res.redirect(301, '/games')
-        },
-        json: function(){
-            res.status(406).send('NOT_API_AVAILABLE')
-        },
-    })
-})
+// app.all('/', (req, res, next) => {
+//     res.format({
+//         html: function(){
+//             res.redirect(301, '/games')
+//         },
+//         json: function(){
+//             throw new NotAcceptableError()
+//         },
+//     })
+// })
 
 // Error Middleware
 //TODO
-router.use((err, req, res, next) => {
+app.use((error, req, res, next) => {  
+    if (!(error instanceof HttpError)) {
+      console.error(error)
+      error = new ServerError()
+    }
+    
     res.format({
-      html: () => {
-        console.log("error : " + err)
-        res.render("error", {
-          error: err
-        })
-      },
-      json: () => {
-        console.log("error : " + err)
-        res.status(500).send(err)
-      }
+        html: () => {
+          res.render("error", {
+            error: error
+          })
+        },
+        json: () => {
+            res.status(error.status || 500).json({ error })
+        }
     })
-  })
+})
 
 
 
-  
-api.listen(PORT)
+
+app.listen(PORT)
 
 console.log(`http://localhost:${PORT}/`)
 
